3e7a5a9a992dfd70f443f54a0f6f0f5da483f826
konstantingoe <konstantingoebler@googlemail.com>
2021-01-15 10:10:12 +0100

    give it a full try but with only sim=4

--- server_run.R
+++ server_run.R
@@ -6,15 +6,14 @@ source("functions.R")
 
 set.seed(1234)
 
-sim = 20 # simulation runs
+sim = 4 # simulation runs
 n <- c(200,200,600) # sample size include high dimension only on cluster
 d <- c(50,250,1500) # dimensionality --> include high dimension (1500) only on cluster 
 n_E <- 200 # sparsity level of the graph: amount of edges we want to introduce 
 t <- .15 # signal strength
 nlam <- 50 # number of tuning parameters for graphical lasso
 #plan(multisession, workers = 20) ## Run in parallel on Linux cluster
-comment_out <- T
-nCores <- 20
+nCores <- 4
 
 mixed_result_1 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[1], d=d[1], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
 latent_result_1 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[1], d=d[1], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
@@ -22,14 +21,19 @@ latent_result_1 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[
 #mixed_result_1 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[1], d=d[1], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
 #latent_result_1 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[1], d=d[1], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
 
-if (comment_out == F) {
-mixed_result_2 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
-latent_result_2 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
+mixed_result_2 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
+latent_result_2 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
 
-mixed_result_3 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
-latent_result_3 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
+#mixed_result_2 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
+#latent_result_2 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[2], d=d[2], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
 
-plan(sequential)
+mixed_result_3 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
+latent_result_3 <- mclapply(mc.cores = nCores, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
+
+#mixed_result_3 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = F, nlam=nlam, matexport = F, countvar = T))
+#latent_result_3 <- future_lapply(future.seed =T, 1:sim, function(k) serverrun(n=n[3], d=d[3], n_E = n_E, latent = T, nlam=nlam, matexport = F))             
+
+#plan(sequential)
 
 output <- list("Mixed with d = 50" = mixed_result_1, "Mixed with d = 250" = mixed_result_2, "Mixed with d = 1500" = mixed_result_3,
                "Latent with d = 50" = latent_result_1, "Latent with d = 250" = latent_result_2, "Latent with d = 1500" = latent_result_3)
@@ -78,22 +82,5 @@ table_3 <- round(as_tibble(rbind(c('polychoric' = mean(extract.result(mixed_resu
 stargazer(table_1, out = "table_1.tex", summary = F, title=paste("Mixed data structure learning of the precision matrix with n=",n[1],"and d=",d[1],"under",sim, "simulation runs."))                    
 stargazer(table_2, out = "table_2.tex", summary = F, title=paste("Mixed data structure learning of the precision matrix with n=",n[2],"and d=",d[2],"under",sim, "simulation runs."))
 stargazer(table_3, out = "table_3.tex", summary = F, title=paste("Mixed data structure learning of the precision matrix with n=",n[3],"and d=",d[3],"under",sim, "simulation runs."))
-
-}
-
-table_1 <- round(as_tibble(rbind(c('polychoric' = mean(extract.result(mixed_result_1, which = "F")),'sd_p' = sd(extract.result(mixed_result_1, which = "F")),
-                                   'latent data'= mean(extract.result(latent_result_1, which = "F")), 'sd_l' = sd(extract.result(latent_result_1, which = "F"))),
-                                 
-                                 c(mean(extract.result(mixed_result_1, which = "FPR")),sd(extract.result(mixed_result_1, which = "FPR")), 
-                                   mean(extract.result(latent_result_1, which = "FPR")),sd(extract.result(latent_result_1, which = "FPR"))),
-                                 
-                                 c(mean(extract.result(mixed_result_1, which = "TPR")),sd(extract.result(mixed_result_1, which = "TPR")), 
-                                   mean(extract.result(latent_result_1, which = "TPR")),sd(extract.result(latent_result_1, which = "TPR"))),
-                                 
-                                 c(mean(extract.result(mixed_result_1, which = "AUC")),sd(extract.result(mixed_result_1, which = "AUC")),
-                                   mean(extract.result(latent_result_1, which = "AUC")),sd(extract.result(latent_result_1, which = "AUC"))))),4)
-
-stargazer(table_1, out = "table_1.tex", summary = F, title=paste("Mixed data structure learning of the precision matrix with n=",n[1],"and d=",d[1],"under",sim, "simulation runs."))                    
-
 
 
