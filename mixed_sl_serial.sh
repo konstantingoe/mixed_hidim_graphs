bb86ab8debcbffac23a24a52797821ed270a03e6
konstantingoe <konstantingoebler@googlemail.com>
2021-01-23 17:11:25 +0100

    try serial run maybe there is indeed an error?

---
+++ mixed_sl_serial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -o /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning/mixed_SL_serial.out
+#SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
+#SBATCH -J "mixed_SL"
+#SBATCH --get-user-env
+#SBATCH --clusters=serial
+#SBATCH --partition=serial_std
+#SBATCH --nodes=1
+#SBATCH --mail-type=end
+#SBATCH --mail-user=konstantin.goebler@tum.de
+#SBATCH --export=NONE
+#SBATCH --time=48:00:00
+
+module load slurm_setup
+
+R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
+export R_LIBS_USER
+
+module load r
+
+cd /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
+
+Rscript serial_run.R
