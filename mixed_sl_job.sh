9e70101ced7e4408b67db9dcf4d916ceac6e7e85
konstantingoe <konstantin.goebler@tum.de>
2021-04-16 09:45:28 +0200

    new try on mpp3

--- mixed_sl_job.sh
+++ mixed_sl_job.sh
@@ -3,10 +3,10 @@
 #SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
 #SBATCH -J "mixed_SL"
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2_tiny
-#SBATCH --partition=cm2_tiny
+#SBATCH --clusters=mpp3
+#SBATCH --partition=mpp3_batch
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=50
 #SBATCH --mail-type=end
 #SBATCH --mail-user=konstantin.goebler@tum.de
 #SBATCH --export=NONE
@@ -14,8 +14,8 @@
 
 module load slurm_setup
 
-R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
-export R_LIBS_USER
+#R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
+#export R_LIBS_USER
 
 module load r
 
