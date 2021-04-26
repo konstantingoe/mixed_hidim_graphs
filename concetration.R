1fdecf5f0e9fb49920f6be9a9e489733c0fff0b0
konstantingoe <konstantin.goebler@tum.de>
2021-04-26 09:59:05 +0200

    100 simulations are too many for this case... try 50

--- concetration.R
+++ concetration.R
@@ -18,4 +18,35 @@ ggsave("example_graph.pdf")
 
 
 
-sum(ell)
+
+
+
+
+
+### here input a simple adjecency matrix! 
+nlam=50
+data <- generate.data(t=.15, n = 200, d = 50, mode = "fan")
+data_0 <- data[[1]]
+Omega <- data[[2]]
+
+rho_latent <- mixed.omega(data_0)
+results_latent <- glasso.results(Sigma=rho_latent, Omega=Omega, nlam=50, n=200, matexport = T, param = .25)
+
+data_mixed <- make.ordinal.general(data_0, namevector = c("binary" = T, "ordinal" = T, "poisson" = T), unbalanced = .2, low = .05, high = .1)
+### learn sample correlation matrix   
+rho <- mixed.omega(data_mixed)
+
+results <- glasso.results(Sigma=rho, Omega=Omega, nlam=50, n=200, matexport = T,  param = .25)
+
+true_adjecency <- abs(Omega) > 0
+latent_adjecency <- abs(results_latent$`Estimate d Precision Matrix`) > 0
+estimated_adjecency <- abs(results$`Estimated Precision Matrix`) > 0
+
+net_true = network(true_adjecency, directed = FALSE)
+net_latent = network(latent_adjecency, directed = FALSE)
+net_estimated = network(estimated_adjecency, directed = FALSE)
+
+ggnet2(net_true, size = 12, label = TRUE, label.size = 5, label.alpha = 0.75)
+ggnet2(net_latent, size = 12, label = TRUE, label.size = 5, label.alpha = 0.75)
+ggnet2(net_estimated, size = 12, label = TRUE, label.size = 5, label.alpha = 0.75)
+
--- server_run.R
+++ server_run.R
@@ -6,12 +6,12 @@ source("functions.R")
 
 set.seed(1221)
 
-sim <- 100 # simulation runs
+sim <- 50 #100 # simulation runs
 n <- c(200,200,300,600) # sample size include high dimension only on cluster
 d <- c(50,250,750, 1500) # dimensionality --> include high dimension (1500) only on cluster 
 n_E <- c(200,250,750,1500) # sparsity level of the graph: amount of edges we want to introduce 
 t <- .15 # signal strength
-nlam <- 50 # number of tuning parameters for graphical lasso
+nlam <- 30 #50 # number of tuning parameters for graphical lasso
 countvar <- F
 latent <- F
 matexport <-  F
