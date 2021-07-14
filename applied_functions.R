d5c32f81cc66a3555d3ac60aa1c233614b326fba
konstantingoe <konstantin.goebler@tum.de>
2021-07-14 13:58:33 +0200

    add nonparanormal

--- applied_functions.R
+++ applied_functions.R
@@ -147,3 +147,109 @@ omega.select.drton <- function(x=x, param = param, n=n, s = s){
 
 
 
+###### nonparanormal extension ######
+
+#### applied function for undirected graph learning
+# data:             can be supplied in any form, and discrete variables will be declared as factors internally
+# verbose:          supplies a notice that no factors are declared when loading the data in
+# nlam:             is the vector length of panalty parameters for the glasso
+# thresholding:     is an boolean variable. If False, then the EBIC of Foygel, Drton (2010) is 
+#                   used (recommended for high dimensional problems) if True then thresholding will be applied (presumably better for medium and low dimensional problems)
+# param:            value of additional penalty term in eBIC, default .1
+# required packages: stats, polycor, glasso, huge, corpcor, matrix 
+
+mixed.nonpara.graph <- function(data = data, verbose = T, nlam = 50, thresholding = F, param = .1){
+  if (sum(sapply(data, is.factor)) == 0 & verbose == T){
+    cat("Warning, there are no factors in the input data.
+        I'm checking your input and declare factors for level(x)<20")
+  }
+  d <- ncol(data)
+  n <- nrow(data)
+  
+  data <- as.data.frame(data)
+  factor_ids <- sapply(data, function(id) length(unique(id)) < 20)
+  data[,factor_ids] <- lapply(data[,factor_ids], factor)
+  
+  rho <- matrix(1,d,d)
+  
+  if (!requireNamespace("polycor", quietly=TRUE))
+    stop("Please install package \"polycor\".")
+  
+  for(i in 1:(d-1)) {
+    for(j in (i+1):d){
+      if (is.numeric(data[,i]) & is.numeric(data[,j])){
+        rho[i,j] <- rho[j,i] <- 2*sin(pi/6 *spearman(data[,i], data[,j]))
+      }
+      if ((is.factor(data[,i]) & is.numeric(data[,j])) |  (is.numeric(data[,i]) & is.factor(data[,j]))) {
+        if (is.factor(data[,j])) {
+          rho[i,j] <- rho[j,i] <- polycor::polyserial(data[,i], data[,j])
+        } else {
+          rho[i,j] <- rho[j,i] <- polycor::polyserial(data[,j], data[,i])
+        }
+      }
+      if (is.factor(data[,i]) & is.factor(data[,j])) {
+        rho[i,j] <- rho[j,i] <- polycor::polychor(data[,i], data[,j])
+      }
+    }
+  }    
+  
+  if (!requireNamespace("stringr", quietly=TRUE))
+    stop("Please install package \"stringr\".")
+  pair <- rho[lower.tri(rho)]
+  if(any(abs(pair) > .9)) 
+    sapply(seq_along(rho[lower.tri(rho)][which(abs(pair) > .95)]), function (k) warning(paste0('Correlation of the pair ', 
+           str_c(as.character(which(rho[lower.tri(rho)][which(abs(pair) > .9)][k] == rho,
+                                    arr.ind = T)[,1]), collapse = ",")),
+                ' is close to boundary. Inverse might be misleading. '))
+  
+  if (!requireNamespace("corpcor", quietly=TRUE))
+    stop("Please install package \"corpcor\".")
+  if (!requireNamespace("Matrix", quietly=TRUE))
+    stop("Please install package \"Matrix\".")
+  
+  ### eigenvalue decomposition and truncate eigenvalues at 0 
+  
+  if (!is.positive.definite(rho)) {
+    initial_mat_singular <- T
+    rho_pd <- as.matrix(nearPD(rho, corr = T, keepDiag = T)$mat)
+  } else {
+    initial_mat_singular <- F
+    rho_pd <- rho
+  }
+  #diag(rho_pd) <- 1
+  
+  
+  if (!requireNamespace("huge", quietly=TRUE))
+    stop("Please install package \"huge\".")
+  
+  #now with rho_pd we have the sample correlation matrix 
+  huge.result <- huge(rho_pd,nlambda=nlam,method="glasso",verbose=FALSE)
+  if (thresholding == T) {
+    Omega_hat <- omega.select(x=huge.result, n=n, param = param)
+  } else if (thresholding == F){
+    if (!requireNamespace("glasso", quietly=TRUE))
+      stop("Please install package \"glasso\".")
+    Omega_hat <- omega.select.drton(x=huge.result, n=n, s = rho_pd, param = param)
+  }
+  number_edges <- edgenumber(Omega_hat)
+  max_degree <- max(sapply(1:d, function(k) (sum(abs(Omega_hat[k,]) > 0) -1))) 
+  
+  adj_estimate <- abs(Omega_hat) > 0
+  
+  output <- list("Estimated Precision Matrix" = Omega_hat, "Adjecency Matrix" = adj_estimate,
+                 "Sample Correlation Matrix" = rho_pd, "Edgenumber" = number_edges, "Max Degree" = max_degree, "initial_mat_singular" = initial_mat_singular)  
+  return(output)
+}
+
+
+spearman <- function(x,y){
+  rankx <- rank(x)
+  ranky <- rank(y)
+  rankmean <- (length(rankx)+1)/2
+  
+  rho <- (sum((rankx - rankmean)*(ranky - rankmean)))/(sqrt(sum((rankx - rankmean)^2)*sum((ranky - rankmean)^2))) 
+  return(rho)
+}
+
+
+
