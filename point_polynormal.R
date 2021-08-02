db6bd32f9ed66995940135b2fb29a514be16026f
konstantingoe <konstantin.goebler@tum.de>
2021-08-02 17:53:33 +0200

    god damn future_apply

--- point_polynormal.R
+++ point_polynormal.R
@@ -23,25 +23,25 @@ if (detectCores() >= 100){
 
 print("Start with d=50, f_j(x) = x")
 
-#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+##plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
 nonpara_comparison_1 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[1], d=d[1], nlam=30, matexport = F,
                           namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                           unbalanced = .5, nonpara = F))
 
-#plan(sequential)
+##plan(sequential)
 
 table_1 <- extract.nonpararesults(nonpara_comparison_1)
 stargazer(table_1, out = "table_1.tex", summary = F, title=paste("Mixed data structure learning comparison n=",n[1],"and d=",d[1],"under",sim, "simulation runs."))                    
 
 print("continue with d=50, f_j(x) = x^3")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_2 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[1], d=d[1], nlam=nlam, matexport = F,
+nonpara_comparison_2 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[1], d=d[1], nlam=nlam, matexport = F,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = T))
-plan(sequential)
+#plan(sequential)
 
 table_2 <- extract.nonpararesults(nonpara_comparison_2)
 stargazer(table_2, out = "table_2.tex", summary = F, title=paste("Mixed data structure learning with $f_j(x) = x^3$, n=",n[1],"and d=",d[1],"under",sim, "simulation runs."))                    
@@ -50,24 +50,24 @@ stargazer(table_2, out = "table_2.tex", summary = F, title=paste("Mixed data str
 
 print("Start with d=250, f_j(x) = x")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_3 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[2], d=d[2], nlam=nlam, matexport = F,
+nonpara_comparison_3 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[2], d=d[2], nlam=nlam, matexport = F,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = F))
-plan(sequential)
+#plan(sequential)
 
 table_3 <- extract.nonpararesults(nonpara_comparison_3)
 stargazer(table_3, out = "table_3.tex", summary = F, title=paste("Mixed data structure learning comparison n=",n[2],"and d=",d[2],"under",sim, "simulation runs."))                    
 
 print("continue with d=250, f_j(x) = x^3")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_4 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[2], d=d[2], nlam=nlam, matexport = F,
+nonpara_comparison_4 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[2], d=d[2], nlam=nlam, matexport = F,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = T))
-plan(sequential)
+#plan(sequential)
 
 table_4 <- extract.nonpararesults(nonpara_comparison_4)
 stargazer(table_4, out = "table_4.tex", summary = F, title=paste("Mixed data structure learning with $f_j(x) = x^3$, n=",n[2],"and d=",d[2],"under",sim, "simulation runs."))                    
@@ -76,24 +76,24 @@ stargazer(table_4, out = "table_4.tex", summary = F, title=paste("Mixed data str
 
 print("Start with d=750, f_j(x) = x")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_5 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[3], d=d[3], nlam=nlam, matexport = F, sparsity = .5,
+nonpara_comparison_5 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[3], d=d[3], nlam=nlam, matexport = F, sparsity = .5,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = F))
-plan(sequential)
+#plan(sequential)
 
 table_5 <- extract.nonpararesults(nonpara_comparison_5)
 stargazer(table_5, out = "table_5.tex", summary = F, title=paste("Mixed data structure learning comparison n=",n[3],"and d=",d[3],"under",sim, "simulation runs."))                    
 
 print("continue with d=750, f_j(x) = x^3")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_6 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[3], d=d[3], nlam=nlam, matexport = F, sparsity = .5,
+nonpara_comparison_6 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[3], d=d[3], nlam=nlam, matexport = F, sparsity = .5,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = T))
-plan(sequential)
+#plan(sequential)
 
 table_6 <- extract.nonpararesults(nonpara_comparison_6)
 stargazer(table_6, out = "table_6.tex", summary = F, title=paste("Mixed data structure learning with $f_j(x) = x^3$, n=",n[3],"and d=",d[3],"under",sim, "simulation runs."))                    
@@ -102,24 +102,24 @@ stargazer(table_6, out = "table_6.tex", summary = F, title=paste("Mixed data str
 
 print("Start with d=1500, f_j(x) = x")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_7 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[4], d=d[4], nlam=nlam, matexport = F, sparsity = .5,
+nonpara_comparison_7 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[4], d=d[4], nlam=nlam, matexport = F, sparsity = .5,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = F))
-plan(sequential)
+#plan(sequential)
 
 table_7 <- extract.nonpararesults(nonpara_comparison_7)
 stargazer(table_7, out = "table_7.tex", summary = F, title=paste("Mixed data structure learning comparison n=",n[4],"and d=",d[4],"under",sim, "simulation runs."))                    
 
 print("continue with d=1500, f_j(x) = x^3")
 
-plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
+#plan(multisession, workers = numCores) ## Run in parallel on Linux cluster
 
-nonpara_comparison_8 <- future_lapply(future.seed = T, 1:sim, function(k) nonparanormal_run(n=n[4], d=d[4], nlam=nlam, matexport = F, sparsity = .5,
+nonpara_comparison_8 <- mclapply(mc.cores = numCores, 1:sim, function(k) nonparanormal_run(n=n[4], d=d[4], nlam=nlam, matexport = F, sparsity = .5,
                                                                                             namevector = c("binary" = T, "ordinal" = T, "poisson" = T),
                                                                                             unbalanced = .5, nonpara = T))
-plan(sequential)
+#plan(sequential)
 
 table_8 <- extract.nonpararesults(nonpara_comparison_8)
 stargazer(table_8, out = "table_8.tex", summary = F, title=paste("Mixed data structure learning with $f_j(x) = x^3$, n=",n[4],"and d=",d[4],"under",sim, "simulation runs."))                    
