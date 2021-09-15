d301fc9056983b4e7bf09d34661043ca1f577310
konstantingoe <konstantin.goebler@tum.de>
2021-09-15 15:35:55 +0200

    run on cm2_tiny at least for the first results

--- mixed_sl_job.sh
+++ mixed_sl_job.sh
@@ -3,9 +3,8 @@
 #SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
 #SBATCH -J "highdim"
 #SBATCH --get-user-env
-#SBATCH --clusters=inter
-#SBATCH --partition=teramem_inter
-#SBATCH --cpus-per-task=50
+#SBATCH --clusters=cm2_tiny
+#SBATCH --cpus-per-task=20
 #SBATCH --nodes=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=konstantin.goebler@tum.de
@@ -14,8 +13,8 @@
 
 module load slurm_setup
 
-#R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
-#export R_LIBS_USER
+R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
+export R_LIBS_USER
 
 module load r
 
