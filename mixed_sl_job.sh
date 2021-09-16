351ea36bb819ac89d4568069b7f5131890385440
konstantingoe <konstantin.goebler@tum.de>
2021-09-16 15:21:50 +0200

    change libpath again

--- mixed_sl_job.sh
+++ mixed_sl_job.sh
@@ -13,8 +13,11 @@
 
 module load slurm_setup
 
-R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
+R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/x86_64-pc-linux-gnu-library/3.6/"
 export R_LIBS_USER
+R_LIBS_SITE="/dss/dsshome1/lrz/sys/spack/release/19.2/opt/x86_avx2/r/3.6.0-gcc-eujetj3/rlib/R/library/"
+export R_LIBS_SITE
+
 
 module load r
 
