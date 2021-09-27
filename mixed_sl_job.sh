31363901bf443f045c160c964aad3dba8ae582b1
konstantingoe <konstantin.goebler@tum.de>
2021-09-27 15:01:19 +0200

    teramem cluster ist partially down... try tomorrow again

--- mixed_sl_job.sh
+++ mixed_sl_job.sh
@@ -3,14 +3,14 @@
 #SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
 #SBATCH -J "mixed_arb"
 #SBATCH --get-user-env
-#SBATCH --clusters=mpp3
-#SBATCH --partition=mpp3_batch
+#SBATCH --clusters=inter
+#SBATCH --partition=teramem_inter
 #SBATCH --cpus-per-task=50
 #SBATCH --nodes=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=konstantin.goebler@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=48:00:00
+#SBATCH --time=200:00:00
 
 module load slurm_setup
 
