fce73719ab4a7feba21953d49724e66884ef6b98
konstantingoe <konstantin.goebler@tum.de>
2022-02-06 16:37:53 +0100

    teramem down again

--- mixed_sl_job.sh
+++ mixed_sl_job.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH -o /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning/mixed_binary_tera.out
+#SBATCH -o /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning/mixed_binary_tiny.out
 #SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
 #SBATCH -J "mixed_binary"
 #SBATCH --get-user-env
-#SBATCH --clusters=inter
-#SBATCH --partition=teramem_inter
-#SBATCH --cpus-per-task=50
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+#SBATCH --ntasks-per-node=10
 #SBATCH --nodes=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=konstantin.goebler@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=240:00:00
+#SBATCH --time=72:00:00
 
 module load slurm_setup
 
