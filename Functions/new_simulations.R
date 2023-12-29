16e351437ed8c2d54927bf3459a3814a4da1386e
konstantingoe <konstantin.goebler@tum.de>
2023-12-29 11:05:10 +0100

    new sim complete

--- Functions/new_simulations.R
+++ Functions/new_simulations.R
@@ -1,11 +1,7 @@
+rm(list = ls())
+
 source("mixed_hidim_graphs/Packages/packages.R")
 
-t <- .15
-n <- 200
-d <- 50
-latent <- FALSE
-nlam <- 30
-countvar <- TRUE
 # function for euclidean norm
 euclid_norm <- function(x) sqrt(sum(x^2))
 edgenumber <- function(mat = mat, cut = 0) {
@@ -15,37 +11,110 @@ edgenumber <- function(mat = mat, cut = 0) {
 }
 
 
-benchmark_run <- function(runs = 100, n = n, d = d, param = .1, nlam = 30, g, namevector = c("binary" = T, "ordinal" = F, "poisson" = F)) {
-    data <- generate_data(n = n, d = d)
-    # data_0 <- data[[1]]
-    data_1 <- data[[2]]
-    omega <- data[[3]]
+binary_benchmark <- function(
+    runs = 100,
+    n = n,
+    d = d,
+    param = .1,
+    nlam = 30,
+    g = \(x) {
+        x
+    },
+    namevector = c("binary" = T, "ordinal" = F, "poisson" = F)) {
+    # result objects
+    within_list <- list("auc" = c(), "frobenius" = c(), "tpr" = c(), "fpr" = c())
+    result_list <- list("oracle" = within_list, "fan" = within_list, "poly" = within_list, "mle" = within_list)
+    for (sim in seq_len(runs)) {
+        data <- generate_data(n = n, d = d, g = g)
+        data_1 <- data[[2]]
+        omega <- data[[3]]
 
-    data <- NULL
+        data <- NULL
 
-    data_mixed <- make_ordinal_general(data_1, namevector = namevector)
+        data_mixed <- make_ordinal_general(data_1, namevector = namevector)
 
-    ### learn sample correlation matrix
-    sigma_oracle <- hatR_poly(data_1)
-    sigma_fan <- hatR_fan(data_mixed)
-    sigma_poly <- hatR_poly(data_mixed)
-    sigma_mle <- hatR_mle(data_mixed)
+        ### learn sample correlation matrix
+        sigma_oracle <- hatR_poly(data_1, verbose = F)
+        sigma_fan <- hatR_fan(data_mixed, verbose = F)
+        sigma_poly <- hatR_poly(data_mixed, verbose = F)
+        sigma_mle <- hatR_mle(data_mixed, verbose = F)
 
-    data_mixed <- NULL
-    corr_list <- list(sigma_oracle, sigma_fan, sigma_poly, sigma_mle)
+        data_mixed <- NULL
+        corr_list <- list(sigma_oracle, sigma_fan, sigma_poly, sigma_mle)
 
-    for (corr_mat in seq_len(length(corr_list))) {
-        omega_path <- huge::huge(corr_mat, nlambda = nlam, method = "glasso", verbose = FALSE)
-        omega_hat <- ebic_selection(x = omega_path, n = n, s = corr_mat, param = param)
+        rep <- 0
+        for (corr_mat in corr_list) {
+            rep <- rep + 1
+            omega_path <- huge::huge(corr_mat, nlambda = nlam, method = "glasso", verbose = FALSE)
+            omega_hat <- ebic_selection(x = omega_path, n = n, s = corr_mat, param = param)
 
-        auc_hat <- auc(truth = omega, huge_obj = omega_path)
-        frobenius <- base::norm(omega_hat - omega, type = "F")
+            result_list[[rep]][["auc"]][sim] <- auc(truth = omega, huge_obj = omega_path)
+            result_list[[rep]][["frobenius"]][sim] <- base::norm(omega_hat - omega, type = "F")
 
-        adj_estimate <- abs(omega_hat) > 0
-        tpr <- tpr(truth = omega, estimate = adj_estimate)
-        fpr <- fpr(truth = omega, estimate = adj_estimate)
+            adj_estimate <- abs(omega_hat) > 0
+            result_list[[rep]][["tpr"]][sim] <- tpr(truth = omega, estimate = adj_estimate)
+            result_list[[rep]][["fpr"]][sim] <- fpr(truth = omega, estimate = adj_estimate)
+            omega_path <- NULL
+            omega_hat <- NULL
+            adj_estimate <- NULL
+        }
     }
+    return(result_list)
 }
+
+
+
+general_benchmark <- function(
+    runs = 100,
+    n = n,
+    d = d,
+    param = .1,
+    nlam = 30,
+    g = \(x) {
+        x
+    },
+    namevector = c("binary" = T, "ordinal" = T, "poisson" = T)) {
+    # result objects
+    within_list <- list("auc" = c(), "frobenius" = c(), "tpr" = c(), "fpr" = c())
+    result_list <- list("oracle" = within_list, "feng" = within_list, "poly" = within_list, "mle" = within_list)
+    for (sim in seq_len(runs)) {
+        data <- generate_data(n = n, d = d, g = g)
+        data_1 <- data[[2]]
+        omega <- data[[3]]
+
+        data <- NULL
+
+        data_mixed <- make_ordinal_general(data_1, namevector = namevector)
+
+        ### learn sample correlation matrix
+        sigma_oracle <- hatR_poly(data_1, verbose = F)
+        sigma_feng <- hatR_fan(data_mixed, verbose = F)
+        sigma_poly <- hatR_poly(data_mixed, verbose = F)
+        sigma_mle <- hatR_mle(data_mixed, verbose = F)
+
+        data_mixed <- NULL
+        corr_list <- list(sigma_oracle, sigma_feng, sigma_poly, sigma_mle)
+
+        rep <- 0
+        for (corr_mat in corr_list) {
+            rep <- rep + 1
+            omega_path <- huge::huge(corr_mat, nlambda = nlam, method = "glasso", verbose = FALSE)
+            omega_hat <- ebic_selection(x = omega_path, n = n, s = corr_mat, param = param)
+
+            result_list[[rep]][["auc"]][sim] <- auc(truth = omega, huge_obj = omega_path)
+            result_list[[rep]][["frobenius"]][sim] <- base::norm(omega_hat - omega, type = "F")
+
+            adj_estimate <- abs(omega_hat) > 0
+            result_list[[rep]][["tpr"]][sim] <- tpr(truth = omega, estimate = adj_estimate)
+            result_list[[rep]][["fpr"]][sim] <- fpr(truth = omega, estimate = adj_estimate)
+            omega_path <- NULL
+            omega_hat <- NULL
+            adj_estimate <- NULL
+        }
+    }
+    return(result_list)
+}
+
 
 identity <- function(x) {
     return(x)
@@ -92,7 +161,7 @@ generate_data <- function(n = 200, d = 50, g = identity) {
     }
     # diag(Omega) <- 1
 
-    edge_number <- edgenumber(Precision = Omega)
+    edge_number <- edgenumber(mat = Omega)
 
     #### retrieve Sigma ####
     Sigma <- Matrix::chol2inv(chol(Omega)) # is simply a lot faster!175
@@ -236,7 +305,7 @@ hatR_fan <- function(data = data, verbose = T) {
                 hatR[i, j] <- hatR[j, i] <- fan_case_2(data[, i], data[, j])
             }
             if (is.factor(data[, i]) && is.factor(data[, j])) {
-                hatR[i, j] <- hatR[j, i] <- fan.case.3(data[, i], data[, j])
+                hatR[i, j] <- hatR[j, i] <- fan_case_3(data[, i], data[, j])
                 ###
             }
         }
@@ -325,7 +394,7 @@ hatR_poly <- function(data = data, verbose = T) {
             warning(
                 paste0(
                     "Correlation of the pair ",
-                    stringr::str_c(as.character(which(hatR[lower.tri(hatR)][which(abs(pair) > .9)][k] == hatR,
+                    stringr::str_c(as.character(which(hatR[lower.tri(hatR)][which(abs(pair) > .98)][k] == hatR,
                         arr.ind = T
                     )[, 1]), collapse = ",")
                 ),
@@ -378,7 +447,7 @@ hatR_mle <- function(data = data, verbose = T) {
             warning(
                 paste0(
                     "Correlation of the pair ",
-                    stringr::str_c(as.character(which(hatR[lower.tri(hatR)][which(abs(pair) > .9)][k] == hatR,
+                    stringr::str_c(as.character(which(hatR[lower.tri(hatR)][which(abs(pair) > .98)][k] == hatR,
                         arr.ind = T
                     )[, 1]), collapse = ",")
                 ),
@@ -410,7 +479,7 @@ spearman <- function(x, y) {
 #### huge_obj: huge object e.g. glasso output
 auc <- function(truth = truth, huge_obj = huge_obj) {
     stopifnot((class(huge_obj) == "huge"))
-
+    nlam <- length(huge_obj$lambda)
     adj_estimate <- lapply(
         seq_len(nlam),
         function(q) abs(huge_obj$icov[[q]]) > 0
@@ -440,8 +509,8 @@ auc <- function(truth = truth, huge_obj = huge_obj) {
     dfp <- c(diff(fp), 0)
     dtp <- c(diff(tp), 0)
 
-    AUC <- sum(tp * dfp) + sum(dtp * dfp) / 2
-    return(AUC)
+    auc_hat <- sum(tp * dfp) + sum(dtp * dfp) / 2
+    return(auc_hat)
 }
 
 tpr <- function(truth = truth, estimate = estimate) {
@@ -488,3 +557,257 @@ ebic_selection <- function(x = x, param = param, n = n, s = s, partial = F) {
     }
     return(Omega_hat.standardized)
 }
+
+
+
+fan_case_2 <- function(x, y) {
+    x <- as.numeric(x)
+    y <- as.numeric(y)
+    tau <- kendall.a(x, y) # need this otherwise Kendall's tau is too expensive here
+    # tau <- cor.fk(x, y) # need this otherwise Kendall's tau is too expensive here
+
+    if (length(unique(x)) == 2) {
+        x.help <- x
+        x.help[x.help == min(x.help)] <- 0
+        x.help[x.help == max(x.help)] <- 1
+        delta_hat <- qnorm(1 - mean(x.help))
+    } else if (length(unique(y)) == 2) {
+        y.help <- y
+        y.help[y.help == min(y.help)] <- 0
+        y.help[y.help == max(y.help)] <- 1
+        delta_hat <- qnorm(1 - mean(y.help))
+    }
+    bridge_func <- function(t) {
+        return(
+            (
+                4 * mvtnorm::pmvnorm(
+                    lower = -Inf,
+                    upper = c(delta_hat, 0),
+                    corr = matrix(c(1, t / sqrt(2), t / sqrt(2), 1),
+                        nrow = 2,
+                        ncol = 2
+                    )
+                )
+                - 2 * pnorm(delta_hat) - tau
+            )^2
+        )
+    }
+    hatR <- tryCatch(
+        expr = {
+            optimize(bridge_func, lower = -0.999, upper = 0.999, tol = 1e-8)[1]
+        },
+        error = function(e) {
+            # message('Caught an error!')
+            # message(e)
+            return(2^(1 / 2) * sin(tau * pi / 2))
+        }
+    )
+    return(as.numeric(hatR))
+}
+
+# kendall's tau with corrections for ties!
+kendall.a <- function(x, y) {
+    x <- as.numeric(x)
+    y <- as.numeric(y)
+    n <- length(x)
+    n0 <- n * (n - 1) / 2
+    n_1 <- n_x(x, n)
+    n_2 <- n_x(y, n)
+    n_sqrt <- sqrt(n0 - c(n_1, n_2))
+    kendall <- pcaPP::cor.fk(x, y) # takes care of ties, so we need to backtransform
+    ties <- prod(n_sqrt) / n0
+    kendall.a <- kendall * ties
+    return(kendall.a)
+}
+
+n_x <- function(x, n) {
+    if (length(unique(x) != n)) {
+        x.info <- rle(sort(x))
+        t_x <- x.info$lengths[x.info$lengths > 1]
+        n_x <- sum(t_x * (t_x - 1) / 2)
+    } else {
+        n_x <- 0
+    }
+    return(n_x)
+}
+
+
+fan_case_3 <- function(x, y) {
+    x <- as.numeric(x)
+    y <- as.numeric(y)
+    tau <- kendall.a(x, y)
+    # tau <- cor.fk(x,y)
+    x.help <- x
+    x.help[x.help == min(x.help)] <- 0
+    x.help[x.help == max(x.help)] <- 1
+
+    y.help <- y
+    y.help[y.help == min(y.help)] <- 0
+    y.help[y.help == max(y.help)] <- 1
+
+    delta_hat_x <- qnorm(1 - mean(x.help))
+    delta_hat_y <- qnorm(1 - mean(y.help))
+
+    bridge_func <- function(t) {
+        return(
+            (
+                2 * mvtnorm::pmvnorm(
+                    lower = c(-Inf, -Inf),
+                    upper = c(delta_hat_x, delta_hat_y),
+                    corr = matrix(c(1, t, t, 1), nrow = 2, ncol = 2)
+                )
+                - 2 * pnorm(delta_hat_x) * pnorm(delta_hat_y) - tau)^2
+        )
+    }
+    hatR <- tryCatch(
+        expr = {
+            optimize(bridge_func, lower = -0.999, upper = 0.999, tol = 1e-8)[1]
+        },
+        error = function(e) {
+            # message('Caught an error!')
+            # message(e)
+            return(sin(pi * tau))
+        }
+    )
+    return(as.numeric(hatR))
+}
+
+
+feng_case_3 <- function(x, y) {
+    #' @param x a vector containing the observations of the 1st ordinal r.v.
+    #' @param y a vector containing the observations of the 2nd ordinal r.v.
+
+    c_x <- length(unique(x))
+    c_y <- length(unique(y))
+    hatR_pq <- matrix(0, c_x, c_y)
+
+    for (p in seq_len(c_x)) {
+        for (q in seq_len(c_y)) {
+            x_p <- 1 * (x >= p)
+            y_q <- 1 * (y >= q)
+            # computing Kendall's tau
+            tau <- pcaPP::cor.fk(x_p, y_q)
+            # estimating thresholds
+            gamma_p <- qnorm(1 - mean(x_p))
+            gamma_q <- qnorm(1 - mean(y_q))
+
+            bridge_func <- function(t) {
+                return(
+                    (
+                        2 * mvtnorm::pmvnorm(
+                            lower = c(-Inf, -Inf),
+                            upper = c(gamma_p, gamma_q),
+                            corr = matrix(c(1, t, t, 1), nrow = 2, ncol = 2)
+                        )
+                        - 2 * pnorm(gamma_p) * pnorm(gamma_q) - tau)^2
+                )
+            }
+            hatR_pq[p, q] <- as.numeric(
+                tryCatch(
+                    expr = {
+                        optimize(bridge_func,
+                            lower = -0.999,
+                            upper = 0.999,
+                            tol = 1e-8
+                        )[1]
+                    },
+                    error = function(e) {
+                        message("Caught an error!")
+                        message(e)
+                        return(sin(pi * tau))
+                    }
+                )
+            )
+        }
+    }
+    # Adding the weights
+    w <- 1 / (c_x * c_y)
+    hatR <- sum(hatR_pq * w)
+    return(hatR)
+}
+
+feng_case_2 <- function(ordinal, continuous) {
+    #' @param ordinal a vector containing the observations of the ordinal r.v.
+    #' @param continuous a vector containing the observations of the continuous r.v.
+    c_ordinal <- length(unique(ordinal))
+    hatR_p <- rep(0, c_ordinal)
+    # delta <- rep(0, c_ordinal)
+    for (p in seq_len(c_ordinal)) {
+        ordinal_p <- 1 * (ordinal >= p)
+        # computing Kendall's tau
+        tau <- pcaPP::cor.fk(ordinal_p, continuous)
+        # estimating thresholds
+        delta_hat <- qnorm(1 - mean(ordinal_p))
+
+        bridge_func <- function(t) {
+            return(
+                (
+                    4 * mvtnorm::pmvnorm(
+                        lower = c(-Inf, -Inf),
+                        upper = c(delta_hat, 0),
+                        corr = matrix(c(1, t / sqrt(2), t / sqrt(2), 1),
+                            nrow = 2,
+                            ncol = 2
+                        )
+                    )
+                    - 2 * pnorm(delta_hat) - tau
+                )^2
+            )
+        }
+        hatR_p[p] <- as.numeric(
+            tryCatch(
+                expr = {
+                    optimize(bridge_func, lower = -0.999, upper = 0.999, tol = 1e-8)[1]
+                },
+                error = function(e) {
+                    message("Caught an error!")
+                    message(e)
+                    return(2^(1 / 2) * sin(tau * pi / 2))
+                }
+            )
+        )
+    }
+    # Adding the weights
+    w <- 1 / c_ordinal
+    hatR <- sum(w * hatR_p)
+    return(hatR)
+}
+
+hatR_feng <- function(data = data, verbose = T) {
+    if (sum(sapply(data, is.factor)) == 0 && verbose == T) {
+        cat("Warning, there are no factors in the input data.
+            Did you declare ordinal variables as factors?")
+    }
+    d <- ncol(data)
+    hatR <- diag(d)
+
+    for (i in 1:(d - 1)) {
+        for (j in (i + 1):d) {
+            if (is.numeric(data[, i]) && is.numeric(data[, j])) {
+                ### Fan et.al.
+                hatR[i, j] <- hatR[j, i] <- sin(pcaPP::cor.fk(data[, i], data[, j]) * pi / 2)
+                ###
+            }
+            if ((is.factor(data[, i]) && is.numeric(data[, j])) || (is.numeric(data[, i]) && is.factor(data[, j]))) {
+                hatR[i, j] <- hatR[j, i] <- feng_case_2(data[, i], data[, j])
+            }
+            if (is.factor(data[, i]) && is.factor(data[, j])) {
+                hatR[i, j] <- hatR[j, i] <- feng_case_3(data[, i], data[, j])
+                ###
+            }
+        }
+    }
+    if (!corpcor::is.positive.definite(hatR)) {
+        hatR_pd <- as.matrix(Matrix::nearPD(hatR, corr = T, keepDiag = T)$mat)
+    } else {
+        hatR_pd <- hatR
+    }
+    return(hatR_pd)
+}
+
+
+
+
+test1 <- binary_benchmark(runs = 2, n = 200, d = 50)
+
+test2 <- general_benchmark(runs = 2, n = 200, d = 50)
