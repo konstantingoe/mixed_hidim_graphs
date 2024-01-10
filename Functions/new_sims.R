3f88366385f83b71a2d6330578fc909695e43005
konstantingoe <konstantin.goebler@tum.de>
2024-01-10 16:31:27 +0100

    only run general results

--- Functions/new_sims.R
+++ Functions/new_sims.R
@@ -3,45 +3,45 @@ rm(list = ls())
 source("/home/goebler/linux/mixed_hidim_graphs/Packages/packages.R")
 source("/home/goebler/linux/mixed_hidim_graphs/Functions/simulation_functions.R")
 
-target_path <- "/home/goebler/linux/scratch/goebler/simulation_results/"
+target_path <- "/home/goebler/linux/scratch/goebler/cubic_results/"
 #### Binary benchamrk #####
 
-# Binary benchmark d = 50
-start_time <- Sys.time()
-binary_50 <- binary_benchmark(runs = 100, n = 200, d = 50)
-end_time <- Sys.time()
-binary_50_json <- jsonlite::toJSON(binary_50)
-write_json(binary_50_json, paste0(target_path, "binary_50.json"))
-binary_50_elapsed <- end_time - start_time
+# # Binary benchmark d = 50
+# start_time <- Sys.time()
+# binary_50 <- binary_benchmark(runs = 100, n = 200, d = 50)
+# end_time <- Sys.time()
+# binary_50_json <- jsonlite::toJSON(binary_50)
+# write_json(binary_50_json, paste0(target_path, "binary_50.json"))
+# binary_50_elapsed <- end_time - start_time
 
-binary_50_cubic <- binary_benchmark(runs = 100, n = 200, d = 50, g = cubic)
-binary_50_cubic_json <- jsonlite::toJSON(binary_50_cubic)
-write_json(binary_50_cubic_json, paste0(target_path, "binary_50_cubic.json"))
+# binary_50_cubic <- binary_benchmark(runs = 100, n = 200, d = 50, g = cubic)
+# binary_50_cubic_json <- jsonlite::toJSON(binary_50_cubic)
+# write_json(binary_50_cubic_json, paste0(target_path, "binary_50_cubic.json"))
 
-# Binary benchmark d = 250
-start_time <- Sys.time()
-binary_250 <- binary_benchmark(runs = 100, n = 200, d = 250)
-end_time <- Sys.time()
-binary_250_json <- jsonlite::toJSON(binary_250)
-write_json(binary_250_json, paste0(target_path, "binary_250.json"))
-binary_250_elapsed <- end_time - start_time
+# # Binary benchmark d = 250
+# start_time <- Sys.time()
+# binary_250 <- binary_benchmark(runs = 100, n = 200, d = 250)
+# end_time <- Sys.time()
+# binary_250_json <- jsonlite::toJSON(binary_250)
+# write_json(binary_250_json, paste0(target_path, "binary_250.json"))
+# binary_250_elapsed <- end_time - start_time
 
-binary_250_cubic <- binary_benchmark(runs = 100, n = 200, d = 250, g = cubic)
-binary_250_cubic_json <- jsonlite::toJSON(binary_250_cubic)
-write_json(binary_250_cubic_json, paste0(target_path, "binary_250_cubic.json"))
+# binary_250_cubic <- binary_benchmark(runs = 100, n = 200, d = 250, g = cubic)
+# binary_250_cubic_json <- jsonlite::toJSON(binary_250_cubic)
+# write_json(binary_250_cubic_json, paste0(target_path, "binary_250_cubic.json"))
 
-# Binary benchmark d = 750
+# # Binary benchmark d = 750
 
-start_time <- Sys.time()
-binary_750 <- binary_benchmark(runs = 100, n = 300, d = 750)
-end_time <- Sys.time()
-binary_750_json <- jsonlite::toJSON(binary_750)
-write_json(binary_750_json, paste0(target_path, "binary_750.json"))
-binary_750_elapsed <- end_time - start_time
+# start_time <- Sys.time()
+# binary_750 <- binary_benchmark(runs = 100, n = 300, d = 750)
+# end_time <- Sys.time()
+# binary_750_json <- jsonlite::toJSON(binary_750)
+# write_json(binary_750_json, paste0(target_path, "binary_750.json"))
+# binary_750_elapsed <- end_time - start_time
 
-binary_750_cubic <- binary_benchmark(runs = 100, n = 300, d = 750, g = cubic)
-binary_750_cubic_json <- jsonlite::toJSON(binary_750_cubic)
-write_json(binary_750_cubic_json, paste0(target_path, "binary_750_cubic.json"))
+# binary_750_cubic <- binary_benchmark(runs = 100, n = 300, d = 750, g = cubic)
+# binary_750_cubic_json <- jsonlite::toJSON(binary_750_cubic)
+# write_json(binary_750_cubic_json, paste0(target_path, "binary_750_cubic.json"))
 
 #### General benchamrk #####
 
