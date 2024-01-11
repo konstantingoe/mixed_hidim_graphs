383ffdfe0fe44856385d5965a0c8107be990adcd
konstantingoe <konstantin.goebler@tum.de>
2024-01-11 18:10:19 +0100

    new shell run

--- Functions/new_sims.R
+++ Functions/new_sims.R
@@ -1,9 +1,9 @@
 rm(list = ls())
 
-source("/home/goebler/linux/mixed_hidim_graphs/Packages/packages.R")
-source("/home/goebler/linux/mixed_hidim_graphs/Functions/simulation_functions.R")
+source("/dss/dsshome1/lxc0A/ge43doh2/Projects/mixed_hidim_graphs/Packages/packages.R")
+source("/dss/dsshome1/lxc0A/ge43doh2/Projects/mixed_hidim_graphs/Functions/simulation_functions.R")
 
-target_path <- "/home/goebler/linux/scratch/goebler/cubic_results/"
+target_path <- "/dss/dsshome1/lxc0A/ge43doh2/Projects/mixed_hidim_graphs/simulation_results"
 #### Binary benchamrk #####
 
 # # Binary benchmark d = 50
@@ -51,10 +51,10 @@ general_50 <- general_benchmark(runs = 100, n = 200, d = 50)
 end_time <- Sys.time()
 general_50_json <- jsonlite::toJSON(general_50)
 write_json(general_50_json, paste0(target_path, "general_50.json"))
-general_50_elapsed <- end_time - start_time
+(general_50_elapsed <- end_time - start_time)
 
 general_50_cubic <- general_benchmark(runs = 100, n = 200, d = 50, g = cubic)
-general_50_cubic_json <- jsonlite::toJSON(general_50_cubic)
+(general_50_cubic_json <- jsonlite::toJSON(general_50_cubic))
 write_json(general_50_cubic_json, paste0(target_path, "general_50_cubic.json"))
 
 
@@ -64,10 +64,10 @@ general_250 <- general_benchmark(runs = 100, n = 200, d = 250)
 end_time <- Sys.time()
 general_250_json <- jsonlite::toJSON(general_250)
 write_json(general_250_json, paste0(target_path, "general_250.json"))
-general_250_elapsed <- end_time - start_time
+(general_250_elapsed <- end_time - start_time)
 
 general_250_cubic <- general_benchmark(runs = 100, n = 200, d = 250, g = cubic)
-general_250_cubic_json <- jsonlite::toJSON(general_250_cubic)
+(general_250_cubic_json <- jsonlite::toJSON(general_250_cubic))
 write_json(general_250_cubic_json, paste0(target_path, "general_250_cubic.json"))
 
 
@@ -77,10 +77,10 @@ general_750 <- general_benchmark(runs = 100, n = 300, d = 750)
 end_time <- Sys.time()
 general_750_json <- jsonlite::toJSON(general_750)
 write_json(general_750_json, paste0(target_path, "general_750.json"))
-general_750_elapsed <- end_time - start_time
+(general_750_elapsed <- end_time - start_time)
 
 general_750_cubic <- general_benchmark(runs = 100, n = 300, d = 750, g = cubic)
-general_750_cubic_json <- jsonlite::toJSON(general_750_cubic)
+(general_750_cubic_json <- jsonlite::toJSON(general_750_cubic))
 write_json(general_750_cubic_json, paste0(target_path, "general_750_cubic.json"))
 
 # # elapsed table
--- Shells/mixed_sl_serial.sh
+++ Shells/mixed_sl_serial.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
-#SBATCH -o /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning/mixed_SL_serial.out
-#SBATCH -D /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
-#SBATCH -J "mixed_SL"
+#SBATCH -o $HOME/Projects/mixed_hidim_graphs/outfiles/serial.out
+#SBATCH -D $HOME/Projects/mixed_hidim_graphs
+#SBATCH -J "mixed_serial"
 #SBATCH --get-user-env
-#SBATCH --clusters=cm2_tiny
-#SBATCH --partition=cm2_tiny
-#SBATCH --cpus-per-task=20
-#SBATCH --nodes=1
+#SBATCH --clusters=serial
+#SBATCH --partition=serial_long
+#SBATCH --mem=2gb
+#SBATCH --cpus-per-task=1
 #SBATCH --mail-type=end
 #SBATCH --mail-user=konstantin.goebler@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=72:00:00
+#SBATCH --time=8-00:00:00
 
 module load slurm_setup
-
-R_LIBS_USER="/dss/dsshome1/lxc0A/ge43doh2/R/debian10/3.6/"
-export R_LIBS_USER
-
 module load r
 
-cd /dss/dsshome1/lxc0A/ge43doh2/mixed/mixed_structure_learning
+cd $HOME/Projects/mixed_hidim_graphs/Functions
 
-Rscript kendall_nonpara.R
+Rscript new_sims.R
